b7d43ae977724312d1bba8b6295b56e7a400dcb6
danieltassomkt <contato@danieltasso.com.br>
2025-05-22 20:51:57 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from pyrogram import Client
-from pyrogram.types import ChatJoinRequest
+from pyrogram.types import ChatJoinRequest, InlineKeyboardMarkup, InlineKeyboardButton
 import os
 
 api_id = int(os.environ["API_ID"])
@@ -12,11 +12,15 @@ app = Client("luisvaz_bot", api_id=api_id, api_hash=api_hash, bot_token=bot_toke
 async def aprovar(client, request: ChatJoinRequest):
     try:
         await client.approve_chat_join_request(request.chat.id, request.from_user.id)
+
         await client.send_message(
             chat_id=request.from_user.id,
-            text="🤖 O que esse bot faz?\nTe mostra o caminho para mudar de vida com o Day Trade, copiando minhas operações passo a passo."
+            text="🤖 O que esse bot faz?\nTe mostra o caminho para mudar de vida com o Day Trade, copiando minhas operações passo a passo.\n\nClique abaixo para começar agora:",
+            reply_markup=InlineKeyboardMarkup(
+                [[InlineKeyboardButton("👉 Acessar Conteúdo Exclusivo", url="https://t.me/luisvazcap_bot?start=w39821122")]]
+            )
         )
-        print(f"✅ Aprovado e mensagem enviada para: {request.from_user.first_name}")
+        print(f"✅ Aprovado e link do ManyChat enviado para: {request.from_user.first_name}")
     except Exception as e:
         print(f"❌ Erro: {e}")
 
